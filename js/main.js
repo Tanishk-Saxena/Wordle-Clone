5d7f388edd708bdf3cc9b0c33b0dde249c6c3d56
Tanishk-Saxena <tanishksaxena686@outlook.com>
2022-04-14 16:20:43 +0530

    added animation after pressing enter

--- js/main.js
+++ js/main.js
@@ -8,12 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let availableSpace = 1;
 
   const word = "dairy";
+  let guessedWordCount = 0;
 
   function createSquares(){
     const gameBoard = document.getElementById("board");
     for (var i = 0; i < 30; i++) {
       let square = document.createElement("div");
       square.classList.add("square");
+      square.classList.add("animate__animated");
       square.setAttribute("id", i+1);
       gameBoard.appendChild(square);
     }
@@ -41,6 +43,18 @@ document.addEventListener("DOMContentLoaded", () => {
       window.alert("Word must be 5 letters");
     }
     const currentWord = currentWordArr.join("");
+    const firstLetterId = guessedWordCount*5+1;
+    const interval = 200;
+    currentWordArr.forEach((letter, index) => {
+      setTimeout(() => {
+        const tileColor = "rgb(58, 58, 60)";
+        const letterId = firstLetterId + index;
+        const letterEl = document.getElementById(letterId);
+        letterEl.classList.add("animate__flipInX");
+        letterEl.style = "background-color:"+tileColor+";border-color:"+tileColor+";";
+      }, interval*index);
+    });
+    guessedWordCount+=1;
     if(currentWord === word){
       window.alert("Congratulations!");
     }
